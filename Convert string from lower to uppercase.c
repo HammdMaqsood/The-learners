e8a4f112f24ccfe91b964fcd1495f39d9f56607c
Hammad <55254990+HammdMaqsood@users.noreply.github.com>
2022-08-28 02:30:38 +0500

    Add files via upload

---
+++ Convert string from lower to uppercase.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<ctype.h>
+int main()
+{
+      char *articles[]={"the","a","one","some","any"};
+      int i,j;
+      i=0;
+      j=2;
+printf("%c",toupper(articles[i][j]));
+
+}
+
+
